2689a33a60abf87a3349dc9436c90a3b9ebc123a
wuzhibin <wuzb@szkingdom.com>
2021-08-16 08:30:23 +0800

    update list

--- Course/Section-1/day15/3.面试题.py
+++ Course/Section-1/day15/3.面试题.py
@@ -2,32 +2,33 @@
 #     for i in range(4):
 #         yield i
 #
-# g=demo()
 #
-# g1=(i for i in g)
-# g2=(i for i in g1)
+# g = demo()
+#
+# g1 = (i for i in g)
+# g2 = (i for i in g1)
 #
 # print(list(g))
 # print(list(g1))
 # print(list(g2))
 
-def add(n,i):
-    return n+i
-
-def test():
-    for i in range(4):
-        yield i
-
-g=test()
-for n in [1,10,5]:
-    g=(add(n,i) for i in g)
+# def add(n,i):
+#     return n+i
+#
+# def test():
+#     for i in range(4):
+#         yield i
+#
+# g=test()
+# for n in [1,10,5]:
+#     g=(add(n,i) for i in g)
+# # print(list(g))
+# # n = 1
+# # g=(add(n,i) for i in test())
+# # n = 10
+# # g=(add(n,i) for i in (add(n,i) for i in test()))
+# # n = 5
+# # g=(15,16,17,18)
+# help(str)
+#
 # print(list(g))
-# n = 1
-# g=(add(n,i) for i in test())
-# n = 10
-# g=(add(n,i) for i in (add(n,i) for i in test()))
-# n = 5
-# g=(15,16,17,18)
-help(str)
-
-print(list(g))
