b60acfe0b61a17d6b930ee388d88e5ceb0c4b3e0
wuzhibin <wuzb@szkingdom.com>
2021-08-23 09:02:54 +0800

    update list

--- Course/Section-1/day16/3.匿名函数.py
+++ Course/Section-1/day16/3.匿名函数.py
@@ -1,16 +1,19 @@
 
-# 两个数的和
-def add(x,y):
-    return x+y
-add = lambda x,y:x+y
-print(add(1,2))
-
-dic={'k1':10,'k2':100,'k3':30}
+# # 两个数的和
+# def add(x,y):
+#     return x+y
+# add = lambda x, y:x+y
+#
+# print(add(1, 2))
+#
+# dic={'k1':10,'k2':100,'k3':30}
 # def func(key):
 #     return dic[key]
 # print(max(dic,key=func))   #根据返回值判断最大值，返回值最大的那个参数是结果
-print(max(dic,key=lambda key:dic[key]))
-print(max([1,2,3,4,5,-6,-7],key=abs))
+# print(max(dic,key=lambda key:dic[key]))
+# print(max([1,2,3,4,5,-6,-7],key=abs))
+#
+# print(max(dic,key=lambda  key:dic[key]))
 
 # ret = map(abs,[-1,2,-3,4])
 # for i in ret:
@@ -47,8 +50,22 @@ print(max([1,2,3,4,5,-6,-7],key=abs))
 # ret = map(lambda t:{t[0]:t[1]},ret)
 # print(list(ret))
 
-#现有两元组(('a'),('b')),(('c'),('d')),
+
+# 现有两元组(('a'),('b')),(('c'),('d')),
 # 请使用python中匿名函数生成列表[{'a':'c'},{'b':'d'}]
+ret = zip((('a'), ('b')), (('c'), ('d')))
+# ------------------------------方式一--------------------------------------
+# def func(tu):
+#     return {tu[0]:tu[1]}
+# res = map(func,ret)
+# print(list(res))
+# ------------------------------方式二--------------------------------------
+res = map(lambda tu: {tu[0]: tu[1]}, ret)
+print(list(res))
+# ------------------------------方式二--------------------------------------
+print(list(map(lambda tu: {tu[0]: tu[1]},
+      zip((('a'), ('b')), (('c'), ('d'))))))
+
 
 # max min sorted filter map
 # 匿名函数 == 内置函数
@@ -61,5 +78,13 @@ print(max([1,2,3,4,5,-6,-7],key=abs))
 #     return [lambda x:i*x for i in range(4)]
 # print([m(2) for m in multipliers()])
 
-
-
+"""
+3.以下代码的输出是什么？请给出答案并解释。
+def multipliers():
+    return [lambda x:i*x for i in range(4)]   # 0,1,2,3
+print([m(2) for m in multipliers()])
+请修改multipliers的定义来产生期望的结果
+"""
+def multipliers():
+    return (lambda x:i*x for i in range(4))   # 0,1,2,3
+print([m(2) for m in multipliers()])
