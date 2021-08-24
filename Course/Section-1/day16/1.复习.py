3b5bf3c5e83b10bb7e77b1ea2e1d0e840843018e
wuzhibin <wuzb@szkingdom.com>
2021-08-24 09:01:25 +0800

    update list

--- Course/Section-1/day16/1.复习.py
+++ Course/Section-1/day16/1.复习.py
@@ -1,3 +1,32 @@
 # 数据类型 ：int bool 。。。
 # 数据结构 : dict list tuple set str
 
+
+
+# print(eval('1+2+3'))
+#
+# code1 = 'for i in range(10):print(i)'
+# complie1 = compile(code1,"",'exec')
+# exec(complie1)
+#
+# import time
+# for i in range(0,101,2):
+#     time.sleep(0.1)
+#     char_num = i//2      #打印多少个'*'
+#     per_str = '\r%s%% : %s\n' % (i, '*' * char_num) if i == 100 else '\r%s%% : %s'%(i,'*'*char_num)
+#     print(per_str,end='', flush=True)
+# t=(1,2,3)
+# print(hash(t))
+#
+
+# l = (1,2,23,213,5612,342,43)
+# def is_old(i):
+#     return i % 2 ==1
+# ret = list(filter(is_old,l))
+# print(ret)
+
+import math
+
+def is_sqrt(x):
+    return math.sqrt(x) % 2 == 0
+print(list(filter(is_sqrt,range(1,101))))
--- Course/Section-1/day16/3.匿名函数.py
+++ Course/Section-1/day16/3.匿名函数.py
@@ -88,3 +88,6 @@ print([m(2) for m in multipliers()])
 def multipliers():
     return (lambda x:i*x for i in range(4))   # 0,1,2,3
 print([m(2) for m in multipliers()])
+
+print(globals())
+print(locals())
